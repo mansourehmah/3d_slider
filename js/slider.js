ea47b1d0229e35c0a44a39a92e02393c28399ac4
mansoureh <mansoureh.1998@gmail.com>
2020-09-19 15:31:52 +0430

    change slide with click

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,8 @@
 $(function () {
   var _w = $(this).innerWidth() / 2;
   var _h = $(this).innerHeight() / 2;
+  var index = 1;
+  //start change cursor
   $(".main").mousemove(function (event) {
     var x, y;
     x = event.clientX;
@@ -39,4 +41,55 @@ $(function () {
   $(".main").mouseleave(function () {
     $(".main>div>span").css("display", "none");
   });
+  //end change cursor
+
+  // start change slide with click page
+  $(".main").click(function (event) {
+    $(".show>div>img").css({
+      "z-index": 19,
+      animation: "none",
+    });
+    if (_w > event.clientX) {
+      $(".show>div>img:nth-of-type(" + index + ")").css({
+        animation: "prev2 1s forwards",
+        "z-index": 20,
+      });
+      if (index > 1) {
+        index--;
+      } else {
+        index = 5;
+      }
+      $(".show>div>img:nth-of-type(" + index + ")").css({
+        animation: "prev 1s forwards",
+        "z-index": 21,
+      });
+    } else {
+      $(".show>div>img:nth-of-type(" + index + ")").css({
+        animation: "next2 1s forwards",
+        "z-index": 20,
+      });
+      if (index < 5) {
+        index++;
+      } else {
+        index = 1;
+      }
+      $(".show>div>img:nth-of-type(" + index + ")").css({
+        animation: "next 1s forwards",
+        "z-index": 21,
+      });
+    }
+    $(".cont>span>span").css({
+      "background-color": "transparent",
+    });
+    $(".cont>span:nth-of-type(" + index + ")>span").css({
+      "background-color": "white",
+    });
+    $(".main").css({
+      "background-image":
+        "url(" +
+        $(".show>div>img:nth-of-type(" + index + ")").attr("src") +
+        ")",
+    });
+  });
+//   end change slide with click page
 });
