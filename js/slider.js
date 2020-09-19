16d27494e5a51eb8d7e19c7840c2a3222e2ec274
mansoureh <mansoureh.1998@gmail.com>
2020-09-19 15:17:51 +0430

    change cursor

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+$(function () {
+  var _w = $(this).innerWidth() / 2;
+  var _h = $(this).innerHeight() / 2;
+  $(".main").mousemove(function (event) {
+    var x, y;
+    x = event.clientX;
+    y = event.clientY;
+    $(".main>div>span").css("display", "none");
+    if (_w < x) {
+      $(".main>div>span:nth-of-type(1)").css({
+        display: "inline",
+        left: x - 15,
+        top: y - 15,
+      });
+    } else {
+      $(".main>div>span:nth-of-type(2)").css({
+        display: "inline",
+        left: x - 15,
+        top: y - 15,
+      });
+    }
+  });
+});
